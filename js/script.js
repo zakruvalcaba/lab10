ae1a815899c3de9630777f8d67b874797665c7d0
zakruvalcaba <zakruvalcaba@gmail.com>
2021-03-25 07:01:09 -0700

    updated lab

--- js/script.js
+++ js/script.js
@@ -1,39 +1,44 @@
-let form = document.getElementById('addForm');
-let itemList = document.getElementById('items');
+// GET FORM AND TASK LIST FROM THE DOM
+let form = document.querySelector('#addForm');
+let itemList = document.querySelector('#items');
 
 // ADD TASK
 form.addEventListener('submit', (e) => {
     // PREVENT FORM SUBMISSION
     e.preventDefault();
     // GET THE VALUE FROM THE TEXT BOX
-    let newItem = document.getElementById('item').value;
+    let newItem = document.querySelector('#item').value;
     // CREATE A NEW LI ELEMENT
     let li = document.createElement('li');
-    // ADD THE .list-group-item CLASS TO THE LI
+    // ADD '.list-group-item' CLASS TO LI
     li.className = 'list-group-item';
-    // ADD A TEXT NODE AND SET IT TO THE INPUT'S VALUE
+    // ADD TEXT NODE AND SET IT TO THE INPUTS VALUE
     li.appendChild(document.createTextNode(newItem));
 
     // CREATE THE DELETE BUTTON
     let deleteBtn = document.createElement('button');
-    // ADD THE NECESSARY CLASSES TO THE DELETE BUTTON
+    // ADD NECESSARY CLASSES TO BUTTON
     deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
-    // APPEND THE 'X' TEXT TO THE DELETE BUTTON
+    // CREATE TEXT NODE, SET IT TO 'X', AND APPEND TO DELETE BUTTON
     deleteBtn.appendChild(document.createTextNode('X'));
-    // APPEND THE DELETE BUTTON TO THE LI
+    // APPEND DELETE BUTTON TO THE LI
     li.appendChild(deleteBtn);
 
-    // APPEND THE LI TO THE LIST
+    // APPEND NEW LI TO THE LIST
     itemList.appendChild(li);
+    // CLEAR THE TEXT BOX
+    document.querySelector('#item').value = '';
+    // SET FOCUS BACK TO TEXT BOX
+    document.querySelector('#item').focus();
 });
 
-// REMOVE ITEM
+// DELETE TASK
 itemList.addEventListener('click', (e) => {
-    // CHECK TO SEE IF .delete CLASS EXISTS ON LI
+    // CHECK TO SEE IF THE .delete CLASS EXISTS ON LI
     if (e.target.classList.contains('delete')) {
-        // CONFIRM THE DELETION
-        if (confirm('Are you sure?')) {
-            // DELETE THE LI
+        // CONFIRM THE DELETE
+        if (confirm('Are you sure you want to delete this task?')) {
+            // SELECT THE PARENT LI ELEMENT AND THEN DELETE IT
             itemList.removeChild(e.target.parentElement);
         }
     }
