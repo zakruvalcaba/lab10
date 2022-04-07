bfeb99c276978897432028dd9604dc77900b6125
Zak Ruvalcaba <zakruvalcaba@gmail.com>
2022-04-06 20:47:45 -0700

    Updated code

--- js/script.js
+++ js/script.js
@@ -1,45 +1,46 @@
 // GET FORM AND TASK LIST FROM THE DOM
-let form = document.querySelector('#addForm');
-let itemList = document.querySelector('#items');
+let form = document.getElementById('addForm')
+let itemList = document.getElementById('items')
 
 // ADD TASK
 form.addEventListener('submit', (e) => {
     // PREVENT FORM SUBMISSION
-    e.preventDefault();
+    e.preventDefault()
     // GET THE VALUE FROM THE TEXT BOX
-    let newItem = document.querySelector('#item').value;
-    // CREATE A NEW LI ELEMENT
-    let li = document.createElement('li');
-    // ADD '.list-group-item' CLASS TO LI
-    li.className = 'list-group-item';
-    // ADD TEXT NODE AND SET IT TO THE INPUTS VALUE
-    li.appendChild(document.createTextNode(newItem));
+    let newItem = document.querySelector('#item').value
+    // CREATE NEW LI ELEMENT
+    let li = document.createElement('li')
+    // ADD 'list-group-item' CLASS TO LI ELEMENT
+    li.className = 'list-group-item'
+    // CREATE TEXT NODE BASED ON INPUT VALUE
+    let text = document.createTextNode(newItem)
+    // APPEND TEXT NODE TO LIST
+    li.appendChild(text)
 
     // CREATE THE DELETE BUTTON
-    let deleteBtn = document.createElement('button');
-    // ADD NECESSARY CLASSES TO BUTTON
-    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+    let deleteBtn = document.createElement('button')
+    // ADD NECESSARY BOOTSTRAP CLASSES FOR BUTTONS
+    deleteBtn.className = 'btn btn-danger btn-sm float-right delete'
     // CREATE TEXT NODE, SET IT TO 'X', AND APPEND TO DELETE BUTTON
-    deleteBtn.appendChild(document.createTextNode('X'));
-    // APPEND DELETE BUTTON TO THE LI
-    li.appendChild(deleteBtn);
+    let textDelete = document.createTextNode('X')
+    deleteBtn.appendChild(textDelete)
+    // APPEND DELETE BUTTON TO LIST ITEM
+    li.appendChild(deleteBtn)
 
-    // APPEND NEW LI TO THE LIST
-    itemList.appendChild(li);
     // CLEAR THE TEXT BOX
-    document.querySelector('#item').value = '';
-    // SET FOCUS BACK TO TEXT BOX
-    document.querySelector('#item').focus();
-});
+    document.querySelector('#item').value = ''
 
-// DELETE TASK
+    // APPEND LI TO LIST
+    itemList.appendChild(li)
+})
+
 itemList.addEventListener('click', (e) => {
     // CHECK TO SEE IF THE .delete CLASS EXISTS ON LI
     if (e.target.classList.contains('delete')) {
-        // CONFIRM THE DELETE
-        if (confirm('Are you sure you want to delete this task?')) {
+        // CONFIRM THE DELETION
+        if (confirm(`Are you sure you want to delete task ${e.target.parentElement.innerText}?`)) {
             // SELECT THE PARENT LI ELEMENT AND THEN DELETE IT
-            itemList.removeChild(e.target.parentElement);
+            itemList.removeChild(e.target.parentElement)
         }
     }
-});
+})
