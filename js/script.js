e08feb5ec48c338d66f7f44bf12de8848b14f9ac
Zak Ruvalcaba <zakruvalcaba@gmail.com>
2023-11-01 19:54:06 -0700

    Updated code

--- js/script.js
+++ js/script.js
@@ -1,23 +1,27 @@
+// GET THE ACCORDION
+let faqs = document.getElementById('faqs')
+// GET ALL THE H2 ELEMENTS FROM THE ACCORDION
+let h2Elements = faqs.getElementsByTagName('h2')
 // ADD EVENT HANDLER TO EACH H2 ELEMENT
 const toggle = (e) => {
-    // GET THE CURRENTLY SELECT H2
+    // GET THE CURRENTLY SELECTED H2
     let h2 = e.currentTarget
-    // GET THE CURREBTLY SELECTED DIV FOR THE H2
+    // GET THE DIV FOR THE CURRENTLY SELECTED H2
     let div = h2.nextElementSibling
 
     // HANDLE REMOVAL OF ALL CLASSES FROM H2 AND DIV ELEMENTS
     // LOOP THROUGH ARRAY OF H2 ELEMENTS
-    for (h2Element of h2Elements) {
+    for (let h2Element of h2Elements) {
         // IF THE H2 ELEMENT IN THE COLLECTION DOESN'T EQUAL THE CURRENTLY
         // CLICKED H2, HIDE ALL CLASS ATTRIBUTES FOR H2 AND SIBLING DIV
         if (h2Element !== e.currentTarget) {
             // REMOVE CLASS FROM H2
             h2Element.removeAttribute('class')
             // REMOVE CLASS FROM SIBLING DIV
-            h2Element.nextElementSibling.removeAttribute('class')
+            h2Element.nextElementSibling.removeAttribute('class')            
         }
     }
-    
+
     // TOGGLE PLUS / MINUS ICON FOR SELECTED H2
     if (h2.hasAttribute('class')) {
         h2.removeAttribute('class')
@@ -32,17 +36,7 @@ const toggle = (e) => {
         div.setAttribute('class', 'open')
     }
 }
-
-// GET THE ACCORDION
-let faqs = document.querySelector('#faqs')
-// GET ALL OF THE H2 ELEMENTS FROM THE ACCORDION
-let h2Elements = faqs.getElementsByTagName('h2')
-// LOOP THROUGH THE COLLECTION OF H2 ELEMENTS AND ATTACH AN EVENT LISTENER TO EACH
-// USING ES5
-// for (let i = 0; i < h2Elements.length; i++) {
-//     h2Elements[i].addEventListener('click', toggle)
-// }
-// USING ES6
+// LOOP THROUGH COLLECTION OF H2 ELEMENTS AND ATTACH TOGGLE EVENT HANDLER
 for (h2Element of h2Elements) {
     h2Element.addEventListener('click', toggle)
 }
